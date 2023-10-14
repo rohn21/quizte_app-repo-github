4c85213f642081c54761caa2b26f23d82ae34415
Rohan_21 <rohn2100@gmail.com>
2023-10-14 17:08:16 +0530

    add url

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('usrs.urls')),
     path('quiz/', include('quiz.urls')),    
+    path('home/', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
--- usrs/urls.py
+++ usrs/urls.py
@@ -8,7 +8,6 @@ urlpatterns=[
      path('register/',TemplateView.as_view(template_name='registration/register.html'), name='register'),
      path('student_register/',views.StudentRegister.as_view(), name='student_register'),
      path('teacher_register/',views.TeacherRegister.as_view(), name='teacher_register'),
-     path('index/', TemplateView.as_view(template_name='index.html'), name='home'),
      path('student/', views.StudentHome.as_view(), name='student'),
      path('teacher/', views.TeacherHome.as_view(), name='teacher'),
      path('login/',views.LoginView.as_view(), name='login'),
